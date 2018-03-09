cd728dd557fb3f227a3feb27ee7f05c1966eb9ed
Ricardo Alexandre Peixoto de Queirós <ricardoqueiros@eseig.pt>
2018-03-09 12:33:39 +0000

    Ficha completa

--- Ficha01/index.js
+++ Ficha01/index.js
@@ -4,26 +4,26 @@ function funcA() {
     console.log("Olá Mundo!")
 }
 
-// Alínea B (JS antigo)
+// Alínea B 
 function funcB() {    
     let name="Ricardo"
     console.log("Olá, " + name + "!")    
 }
 
-// Alínea B (JS6)
+// Alínea B 
 function funcB_2() {    
     let f = function(name) {console.log("Olá, " + name + "!")}
     f("Ricardo")
 }
 
-// Alínea C (JS antigo)
+// Alínea C
 function funcC() {    
     let a = 12; let b = 3
     let comprimento = a * b
     console.log("Comprimento: " + comprimento)    
 }
 
-// Alínea D (JS antigo)
+// Alínea D 
 function funcD() {    
     let n1 = parseInt(prompt("Escreva o 1ª número?"))
     let n2 = parseInt(prompt("Escreva o 2ª número?"))    
@@ -51,7 +51,7 @@ function funcD() {
     }
 }
 
-// Alínea E (JS antigo)
+// Alínea E 
 function funcE() {    
     let altura = parseFloat(prompt("Altura?"))
     let peso = parseFloat(prompt("Peso?"))    
@@ -72,7 +72,7 @@ function funcE() {
     }
 }
 
-// Alínea F (JS6)
+// Alínea F 
 function funcF() {    
     let str = prompt("Escreva uma palavra?")
     let n = parseInt(prompt("Escreva o nº de vezes que quer imprimir a palavra?"))    
@@ -86,7 +86,7 @@ function funcF() {
     f(str,n)
 }
 
-// Alínea G (JS antigo)
+// Alínea G 
 function funcG() {    
     let n1 = parseInt(prompt("Escreva o 1º número?"))
     let n2 = parseInt(prompt("Escreva o 2º número?"))
@@ -96,7 +96,7 @@ function funcG() {
     }
 }
 
-// Alínea H (JS6)
+// Alínea H 
 function funcH() {    
     let num = parseInt(prompt("Escreva o número para calcular a tabuada?"))
     
@@ -105,7 +105,7 @@ function funcH() {
     }
 }
 
-// Alínea I (JS6)
+// Alínea I 
 function funcI() {    
     let n1 = parseInt(prompt("Escreva o 1º número?"))
     let n2 = parseInt(prompt("Escreva o 2º número?"))
@@ -118,7 +118,7 @@ function funcI() {
     console.log("Resultado da soma dos múltiplos de 3: " + result)
 }
 
-// Alínea J (JS6)
+// Alínea J 
 function funcJ() {    
     let n = parseInt(prompt("Escreva um número"))
     let primo = true
@@ -126,13 +126,12 @@ function funcJ() {
         if(n % i == 0) {
             primo = false
         }        
-    }
-    console.log(primo)
+    }    
     let result = primo ? "SIM" : "NÃO"
     console.log(result)
 }
 
-// Alínea K (JS6)
+// Alínea K 
 function funcK() {    
     let n = parseInt(prompt("Escreva um número"))
     let fact = 1    
@@ -142,15 +141,84 @@ function funcK() {
     console.log("O fatorial é " + fact)
 }
 
-// Alínea L (JS6)
+// Alínea L
 function funcL() {    
     let n = parseInt(prompt("Escreva um número"))
-    let primo = true
-    for (let i = 2; i < n; i++) {
+    let total = 0
+    for (let i = 1; i < n; i++) {
         if(n % i == 0) {
-            primo = false
+            total += i
         }        
     }
-    let result = (primo) ? "SIM" : "NÃO"
+    let result = (total == n) ? "SIM" : "NÃO"
     console.log(result)
+}
+
+// Alínea M
+function funcM() {    
+    let ano = parseInt(prompt("Escreva um ano"))
+    let bissexto = false
+    if ( ( ano % 4 == 0 && ano % 100 != 0 ) || (ano % 400 == 0) ) { 
+        bissexto = true
+    }
+    let result = bissexto ? "SIM" : "NÃO"
+    console.log(result)
+}
+
+// Alínea N
+function funcN() { 
+    let num   
+    num = prompt("Escreva um número")        
+    if (num.charAt(0) == num.charAt(2) ) {
+        console.log("SIM")
+    } else {
+        console.log("NÃO")
+    }        
+}
+
+function funcN2() { 
+    let num   
+    do {
+        num = parseInt(prompt("Escreva um número"))
+        
+        if (num>=100 && num<=999) {
+            let num1 = parseInt(num % 10)
+            let num2 = parseInt((num/10) % 10)
+            let num3 = parseInt((num/10) / 10)
+            let newNum = "" + num1 + num2 + num3            
+            if (num.toString() == newNum) {
+                console.log("SIM")
+            } else {
+                console.log("NÃO")
+            }
+        } else {
+            console.log("Nº INVÁLIDO")
+        }    
+    } while (num<100 || num>999);
+}
+
+// Alínea O
+function funcO() {    
+    // Gerar nº aleatório entre 1 e 100
+    let genNum = parseInt((Math.random() * 99) + 1)
+    let attempts = 0
+
+    do {
+        num = parseInt(prompt("Escreva um número"))
+        if (num > genNum) {
+            console.log("PARA BAIXO!")
+        } else if (num < genNum) {
+            console.log("PARA BAIXO!")
+        } else {            
+            break
+        }
+        attempts++
+    } while (attempts < 5);
+    
+    if (attempts < 5) {
+        console.log("ADIVINHOU PARABÉNS!")
+    } else {
+        console.log("EXCEDEU O Nº DE TENTATIVAS!")
+    }
+    
 }
