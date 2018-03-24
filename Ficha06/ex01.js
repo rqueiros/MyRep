1f5ff1f93aa439652776d228da5b9dc9bb6a6603
rqueiros <ricardo.queiros@gmail.com>
2018-03-24 08:36:38 +0000

    Exercícios 1 e 2 completos

--- Ficha06/ex01.js
+++ Ficha06/ex01.js
@@ -4,17 +4,20 @@ window.onload = function() {
 
 
 function init() {
-    // Alteração inciial do texto do parágrafo
+    // Obtenção das referências para os elementos HTML    
     let myP = document.getElementById("myP")
+    let myTxt = document.getElementById("myTxt")
+    let myBtn = document.getElementById("myBtn")
+    
+    // Alteração do texto do elemento parágrafo
     myP.innerHTML = "Bem-vindo à minha página!"
 
-    // Clique no botão
-    let myBtn = document.getElementById("myBtn")
+    // Clique no botão    
     myBtn.addEventListener("click", function() {
-        let myTxt = document.getElementById("myTxt")        
-        console.log(myTxt.value)
+        // Se a caixa de texto estiver vazia: adiciona no parágrafo o texto que está na caixa de texto                
         if (myTxt.value != "") {
             myP.innerHTML = "Bem-vindo, " + myTxt.value + "!"            
+        // Se a caixa de texto não estiver vazia: adiciona no parágrafo o texto original                
         } else {
             myP.innerHTML = "Bem-vindo à minha página!"            
         }
---
+++ Ficha06/ex02.js
@@ -0,0 +1,53 @@
+window.onload = function() {
+    init()    
+}
+
+
+function init() {
+    // Obtenção das referências para os elementos HTML    
+    let btnTodas = document.getElementById("btnTodas")
+    let btnCampus1 = document.getElementById("btnCampus1")
+    let btnCampus2 = document.getElementById("btnCampus2")
+    let btnCampus3 = document.getElementById("btnCampus3")
+
+    // Clique no botão TODAS
+    btnTodas.addEventListener("click", function () {
+        // Obtém referência para a coleção de todos os elementos input
+        let refInputs = document.getElementsByTagName("input")
+        // Itera sobre todos os elementos input e coloca o seu fundo a verde
+        for (let i = 0; i < refInputs.length; i++) {
+            refInputs[i].style.backgroundColor = "green"            
+        }
+    })
+
+    // Clique no botão CAMPUS I
+    btnCampus1.addEventListener("click", function () {
+        // Obtém referência para a coleção de todos os elementos input
+        let refInputs = document.getElementsByClassName("campus1")
+        // Itera sobre todos os elementos input e coloca o seu fundo a verde
+        for (let i = 0; i < refInputs.length; i++) {
+            refInputs[i].style.backgroundColor = "green"            
+        }
+    })
+
+    // Clique no boão TODAS
+    btnCampus2.addEventListener("click", function () {
+        // Obtém referência para a coleção de todos os elementos input
+        let refInputs = document.getElementsByClassName("campus2")
+        // Itera sobre todos os elementos input e coloca o seu fundo a verde
+        for (let i = 0; i < refInputs.length; i++) {
+            refInputs[i].style.backgroundColor = "green"            
+        }
+    })
+
+    // Clique no boão TODAS
+    btnCampus3.addEventListener("click", function () {
+        // Obtém referência para a coleção de todos os elementos input
+        let refInputs = document.getElementsByClassName("campus3")
+        // Itera sobre todos os elementos input e coloca o seu fundo a verde
+        for (let i = 0; i < refInputs.length; i++) {
+            refInputs[i].style.backgroundColor = "green"            
+        }
+    })
+    
+}
