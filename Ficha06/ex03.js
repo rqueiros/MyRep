2c7a8a8efd33b212a3bd5c885d74383ed31c14dd
rqueiros <ricardo.queiros@gmail.com>
2018-04-02 08:41:59 +0100

    Ficha completa

--- Ficha06/ex03.js
+++ Ficha06/ex03.js
@@ -2,6 +2,7 @@ let filmes = []
 
 class Filme {
     constructor(titulo, ano, genero) {
+        this.id = Filme.generateId()
         this.titulo = titulo
         this.ano = ano
         this.genero = genero
@@ -33,6 +34,18 @@ class Filme {
     set genero(novoGenero) {
         this._genero = novoGenero
     }
+
+    static generateId() {
+        let maior = 0
+        for (let i = 0; i < filmes.length; i++) {
+            if(filmes[i].id > maior) {
+                maior = filmes[i].id
+            }            
+        }
+        return maior + 1
+    }
+
+
 }
 
 // Evento executado quando a página HTML for carregada e a árvore DOM estar disponível
@@ -68,14 +81,29 @@ window.onload = function() {
 function atualizaTabela() {
     let tblFilmes = document.getElementById("tblFilmes")
     let str = ""
-    str = "<tr><th>TÍTULO</th><th>ANO</th><th>GÉNERO</th></tr>"
+    str = "<thead class='thead-dark'><tr><th>TÍTULO</th><th>ANO</th><th>GÉNERO</th><th>ACÕES</th></tr></thead><tbody>"
     for (let i = 0; i < filmes.length; i++) {
         str += "<tr>"        
         str += "<td>" + filmes[i].titulo + "</td>"
         str += "<td>" + filmes[i].ano + "</td>"
         str += "<td>" + filmes[i].genero + "</td>"
+        str += "<td><a class='remove'><i class='fas fa-trash-alt'></i></a></td>"
         str +="</tr>"
     }
+    str += "<tbody>"
     tblFilmes.innerHTML = str
+
+    // Obtem todos os <td> da tabela (coluna com o ícone de remoção)
+    let tdRemove = document.getElementsByClassName("remove")
+    // Para cada um deles adiciona um listener para o evento clique
+    for (let i = 0; i < tdRemove.length; i++) {
+        tdRemove[i].addEventListener("click", function() {
+            // Ao clicar num determinado filme, remove-o do array
+            filmes.splice(i, 1)
+            // Atualiza de novo a tabela
+            atualizaTabela()
+        })        
+    }
 }
+
 
